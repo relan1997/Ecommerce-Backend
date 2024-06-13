9641e5474dbae7fdc7309b8a2c4c1190edd756c9
relan1997 <harshal.relan.201116@gmail.com>
2024-06-14 00:45:36 +0530

    added the Order Item Model

---
+++ src/models/orderItems.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+    product:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'products',
+        required:true
+    },
+    size:{
+        type:String,
+    },
+    quantity:{
+        type:Number,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    discountedPrice:{
+        type:Number,
+        required:true
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users',
+        required:true
+    },
+    deliveryType:{
+        type:Date
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+})
+
+const OrderItem = mongoose.model('orderItems',orderItemSchema)
+export default OrderItem
