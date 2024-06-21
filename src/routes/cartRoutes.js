71265d4e32e28fa23fb79e39e48b932132b00694
relan1997 <harshal.relan.201116@gmail.com>
2024-06-22 00:30:20 +0530

    added cart routes

---
+++ src/routes/cartRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+const cartRouter = express.Router();
+import { addItemToCart, findUsersCart } from "../../controllers/cartController";
+import { authenticate } from "../middleware/authenticate";
+
+cartRouter.get("/",authenticate, findUsersCart);
+cartRouter.put("/add", authenticate, addItemToCart);
