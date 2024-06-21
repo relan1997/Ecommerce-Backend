61e804ceacda52d1d916bee27f510a230465b2f3
relan1997 <harshal.relan.201116@gmail.com>
2024-06-21 23:09:37 +0530

    cartItem controller created

---
+++ controllers/cartItemController.js
@@ -0,0 +1,31 @@
+import {
+  findCartItemById,
+  removeCartItem,
+  updateCartItem,
+} from "../services/cartItemService";
+
+const updatedCartItem = async (req, res) => {
+  const { user } = req;
+  try {
+    const CartItem = await updateCartItem(
+      user._id,
+      req.params.id,
+      req.body
+    );
+    return res.status(200).send(CartItem);
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+const removedCartItem = async (req, res) => {
+  const { user } = req;
+  try {
+    await removeCartItem(user._id, req.params.id);
+    return res.status(200).send("cart item removed successfully");
+  } catch (error) {
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+export {updatedCartItem,removedCartItem}
